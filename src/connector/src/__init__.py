7d1828fc2df4ea81d9c4346a5fa9acd9133ae5d7
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-01 01:21:40 +0300

    publisher

--- src/connector/src/__init__.py
+++ src/connector/src/__init__.py
@@ -2,9 +2,13 @@
 
 import eventlet
 import socketio
+import rospy
+from geometry_msgs.msg import Point
 
 sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio)
+poi_pub = rospy.Publisher('points', Point)
+rospy.init_node('connector')
 
 
 @sio.event
@@ -20,6 +24,8 @@ def disconnect(sid):
 @sio.on('point')
 def get_point(sid, data):
     print(sid, data)
+    point_msg = Point()
+    poi_pub.publish(point_msg)
 
 
 print('CatchBot listens on 8765 port :)')
