201813ae91a18b9cca22836d4e615a6e5e8769fc
dropbled <nikramm@yandex.ru>
2020-04-15 14:19:19 +0500

    Ya NIKITA

--- src/runner/src/nav.py
+++ src/runner/src/nav.py
@@ -33,7 +33,113 @@ right = Motor(config_2)
 
 
 def get_point_handler(point):
-	# ТВОЙ КОД ЗДЕСЬ
+	#константы
+	Y = 200
+	X = 200
+	H = 22.5
+	Rk = 3.25
+	#получение траектории 
+	x1 = point.x
+	y1 = point.y
+	while x1 == point.x and y1 == point.y:
+		time.sleep(0.1)
+	x2 = point.x
+	y2 = point.y
+	xn = X / 2
+	yn = Y / 2
+	#получение точки перпендикуляра
+	x4=((x2-x1)*(y2-y1)*(yn-y1)+x1*pow(y2-y1, 2)+xn*pow(x2-x1, 2))/(pow(y2-y1, 2)+pow(x2-x1, 2))
+	y4=(y2-y1)*(x4-x1)/(x2-x1)+y1
+
+	Tx = xn - x4
+	Ty = yn - y4
+#Поиск длины пути до цели
+	D = math.sqrt(Tx**2 + Ty**2)
+#Поиск кратчайшего угла поворота
+	A = math.atan(np.abs(Tx)/np.abs(Ty))
+	if Ty > 0:
+		A = np.pi - A
+	if x4 > xn:
+		A = A * (-1)
+
+	Angle = A * H * 180 / (2 * Rk * np.pi)
+	D_angle = 0
+
+	e = Angle
+	mist = 15 #ошибка энкодера
+	while np.abs(e) > mist :
+		u = e / Angle
+		if A > 0:
+			ur = u
+			ul = -1 * u
+		else:
+			ur = u
+			ul = -1 * u
+		lel = left.get_angle
+		rel = right.get.angle
+		left.go(ul)
+		right.go(ur)
+		left.update()
+		right.update()
+		le = left.get_angle
+		re = right.get.angle
+		D_right = rel - re
+		D_left = lel - le
+		e -= np.abs(D_right) #Или D_right, если они равны. Должны быть равны, иначе пока хз как это считать
+	left.stop()
+	right.stop()
+
+	s = 0
+	while D - s > 0:
+		u = (D - s) / D
+		lel = left.get_angle
+		rel = right.get.angle
+		left.go(u)
+		right.go(u)
+		left.update()
+		right.update()
+		le = left.get_angle
+		re = right.get.angle
+		D_right = rel - re
+		D_left = lel - le
+		s = 2 * np.pi * Rk * D_right/360
+	left.stop()
+	right.stop()
+
+	if ((y2 - y1) * (x4 - xn) - (x2 - x1) * (y4 - yn)) > 0:
+		A = 90
+	elif ((y2 - y1) * (x4 - xn) - (x2 - x1) * (y4 - yn)) < 0:
+		A = -90
+
+	Angle = A * H * 180 / (2 * Rk * np.pi)
+	e = Angle
+	mist = 15 #ошибка энкодера
+	while np.abs(e) > mist :
+		u = e / Angle
+		if A > 0:
+			ur = u
+			ul = -1 * u
+		else:
+			ur = u
+			ul = -1 * u
+		lel = left.get_angle
+		rel = right.get.angle
+		left.go(ul)
+		right.go(ur)
+		left.update()
+		right.update()
+		le = left.get_angle
+		re = right.get.angle
+		D_right = rel - re
+		D_left = lel - le
+		e -= np.abs(D_right) #Или D_right, если они равны. Должны быть равны, иначе пока хз как это считать
+	left.stop()
+	right.stop()
+
+	
+
+
+  # ТВОЙ КОД ЗДЕСЬ
   # КОГДА Я ПРИШЛЮ ТОЧКУ ОНА БУДЕТ ЗДЕСЬ point
   # ТЫ МОЖЕШЬ ПОЛУЧИТЬ КООРДИНАТЫ point.x, point.y and point.z (z ВСЕГДА 0)
   # ХЕНДЛЕР АСИНХРОННЫЙ. ЭТО ЗНАЧИТ, ЧТО ЕСЛИ ТЫ ПОЛУЧИШЬ НОВУЮ ТОЧКУ
