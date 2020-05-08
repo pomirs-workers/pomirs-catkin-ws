b100b4a10e784f93bcb90ce7a1ecb16592b56439
Vlad Ivanov <vivanov@metaprime.ru>
2020-05-08 12:29:24 +0300

    refactor

--- src/runner/src/turn_n_go.py
+++ src/runner/src/turn_n_go.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import Point
 from lib.catch_bot.motor import Motor
 from math import atan, sqrt, pi, fabs
+import time
 
 Motor.apply_requirements()
 Motor.unlock_all()
@@ -34,14 +35,16 @@ right = Motor(config_2)
 x0 = 20
 y0 = 20
 rk = 6
+deg2rad = 0.0174533
 
 def get_arc(angle):
   return (2 * rk * pi) * (angle / (2 * pi))
 
 def handle(value):
-  while fabs(get_arc(left.get_angle())) < value:
+  while fabs(get_arc(left.get_angle() * deg2rad)) < value:
     left.update()
     right.update()
+    time.sleep(0.05)
   left.stop()
   right.stop()
   left.reset()
@@ -53,19 +56,14 @@ def get_point_handler(point):
   angle = atan((y - y0) / (x - x0))
   dist = sqrt((x - x0) ** 2 + (y - y0) ** 2)
   arc = get_arc(angle)
-
   coef = 1
   if angle < 0:
     coef = -1
-
   left.go(0.3 * coef)
   right.go(0.3 * -coef)
-
   handle(arc)
-
   left.go(1)
   right.go(1)
-
   handle(dist)
 
 
